fe6a5cbd54a0b2448942ff6142af9a7cec0ac2a0
BarryYin <1509008060@qq.com>
2024-05-10 14:44:34 +0800

    0510

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Building-Agentic-RAG-with-Llamaindex
 from deeplearning.ai by Jerry Liu, who is CEO at LlamaIndex
+
+# 介绍
+
+本课程来自deeplearning.ai
+本人自学所用
+会有部分注释、笔记、拓展
+
+# 关于课程
+该课程详细的介绍了Llamaindex是如何执行RAG的，从执行效果来看，Llamaindex已经配置了整套的RAG相关的文档加载、Embedding量化、以及查询和总结。
+并且设定了Agent模式，可以自动依据查询的内容，自主选择最佳工具执行。
+
+1. L0_ready.jpynb 检查配置文件
+2. L1_Router_Engine.ipynb 自动选择调用查询引擎
+3. L3_Building_an_Agent_Reasoning_Loop.ipynb 设置一个Agent，reason+rag
+4. L4_Building_a_Multi-Document_Agent.ipynb 复杂的多文档Agent
+
+
+# 更多资源
+1. Custom agent:
+https://docs.llamaindex.ai/en/stable/examples/agent/custom_agent/
+
+2. Community-built Agents (LlamaHub):
+https://llamahub.ai/?tab=agent
+
+3. Advanced document parsing with LlamaParse:
+https:/cloud.llamaindex.ai/
+
+# 点赞
+如果你觉得本pr不错，请给个Star
+
---
+++ helper.py
@@ -0,0 +1,16 @@
+# Add your utilities or helper functions to this file.
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# these expect to find a .env file at the directory above the lesson.                                                                                                                     # the format for that file is (without the comment)                                                                                                                                       #API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService                                                                                                                                     
+def load_env():
+    _ = load_dotenv(find_dotenv())
+
+def get_openai_api_key():
+    load_env()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    print(openai_api_key)
+    return openai_api_key
+
+check = get_openai_api_key()
